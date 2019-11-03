6228a97b3ad8f7065c224d48242b14aabc0baa55
AWS Container Day <56585892+anz-containers@users.noreply.github.com>
2019-11-03 13:41:52 +1100

    Delete eks_config.sh

--- eks_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "# update kube config"
-aws eks update-kubeconfig --name EKS --region us-east-1
-
-echo " # kubectl get nodes"
-kubectl get nodes 
-
-echo ">> Update rbac_worker.yaml, add a new map role for every WorkerNode group Execute: kubectl apply -f ./rbac_worker_nodes.yaml"
